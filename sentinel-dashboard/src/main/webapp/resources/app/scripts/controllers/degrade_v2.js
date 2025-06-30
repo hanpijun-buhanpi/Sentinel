497acc9c2c79914fe94d4efd15cef742c4e5d03c
hanpijun <1548749669@qq.com>
2025-06-30 10:28:07 +0800

    修复机器全部失联后无法配置熔断规则问题

--- sentinel-dashboard/src/main/webapp/resources/app/scripts/controllers/degrade_v2.js
+++ sentinel-dashboard/src/main/webapp/resources/app/scripts/controllers/degrade_v2.js
@@ -1,7 +1,7 @@
 var app = angular.module('sentinelDashboardApp');
 
-app.controller('DegradeControllerV2', ['$scope', '$stateParams', 'DegradeServiceV2', 'ngDialog', 'MachineService',
-  function ($scope, $stateParams, DegradeService, ngDialog, MachineService) {
+app.controller('DegradeControllerV2', ['$scope', '$stateParams', 'DegradeServiceV2', 'ngDialog',
+  function ($scope, $stateParams, DegradeService, ngDialog) {
     //初始化
     $scope.app = $stateParams.app;
     $scope.rulesPageConfig = {
@@ -10,27 +10,9 @@ app.controller('DegradeControllerV2', ['$scope', '$stateParams', 'DegradeService
       totalPage: 1,
       totalCount: 0,
     };
-    $scope.macsInputConfig = {
-      searchField: ['text', 'value'],
-      persist: true,
-      create: false,
-      maxItems: 1,
-      render: {
-        item: function (data, escape) {
-          return '<div>' + escape(data.text) + '</div>';
-        }
-      },
-      onChange: function (value, oldValue) {
-        $scope.macInputModel = value;
-      }
-    };
     getMachineRules();
     function getMachineRules() {
-      if (!$scope.macInputModel) {
-        return;
-      }
-      var mac = $scope.macInputModel.split(':');
-      DegradeService.queryMachineRules($scope.app, mac[0], mac[1]).success(
+      DegradeService.queryMachineRules($scope.app).success(
         function (data) {
           if (data.code == 0 && data.data) {
             $scope.rules = data.data;
@@ -67,12 +49,9 @@ app.controller('DegradeControllerV2', ['$scope', '$stateParams', 'DegradeService
     };
 
     $scope.addNewRule = function () {
-      var mac = $scope.macInputModel.split(':');
       $scope.currentRule = {
         grade: 0,
         app: $scope.app,
-        ip: mac[0],
-        port: mac[1],
         limitApp: 'default',
         minRequestAmount: 5,
         statIntervalMs: 1000,
@@ -176,36 +155,4 @@ app.controller('DegradeControllerV2', ['$scope', '$stateParams', 'DegradeService
         }
       });
     }
-    queryAppMachines();
-    function queryAppMachines() {
-      MachineService.getAppMachines($scope.app).success(
-        function (data) {
-          if (data.code === 0) {
-            // $scope.machines = data.data;
-            if (data.data) {
-              $scope.machines = [];
-              $scope.macsInputOptions = [];
-              data.data.forEach(function (item) {
-                if (item.healthy) {
-                  $scope.macsInputOptions.push({
-                    text: item.ip + ':' + item.port,
-                    value: item.ip + ':' + item.port
-                  });
-                }
-              });
-            }
-            if ($scope.macsInputOptions.length > 0) {
-              $scope.macInputModel = $scope.macsInputOptions[0].value;
-            }
-          } else {
-            $scope.macsInputOptions = [];
-          }
-        }
-      );
-    };
-    $scope.$watch('macInputModel', function () {
-      if ($scope.macInputModel) {
-        getMachineRules();
-      }
-    });
   }]);
--- sentinel-dashboard/src/main/webapp/resources/app/scripts/services/degrade_service_v2.js
+++ sentinel-dashboard/src/main/webapp/resources/app/scripts/services/degrade_service_v2.js
@@ -1,11 +1,9 @@
 var app = angular.module('sentinelDashboardApp');
 
 app.service('DegradeServiceV2', ['$http', function ($http) {
-  this.queryMachineRules = function (app, ip, port) {
+  this.queryMachineRules = function (app) {
     var param = {
-      app: app,
-      ip: ip,
-      port: port
+      app: app
     };
     return $http({
       url: '/v2/degrade/rules.json',
